6d768365fa3bc96a895861783c978795d43703df
oleksandr <oleksandr@fulcrum.rocks>
2022-08-18 20:48:11 +0300

    install brew if not installed

--- brew/brew.sh
+++ brew/brew.sh
@@ -3,6 +3,8 @@
 SCPT_HOME="${HOME}/git/configs/brew/"
 cd "${SCPT_HOME}"
 
+which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 brew tap Homebrew/bundle
 
 if [ -f Brewfile ]
